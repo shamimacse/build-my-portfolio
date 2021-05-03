21b39c2a334fb08a72333f83ff16e4edfcbaf421
Shamima Akter <76779277+shamimacse@users.noreply.github.com>
2021-05-03 22:57:04 +0600

    Add files via upload

---
+++ script-2.js
@@ -0,0 +1,12 @@
+function sendMail(params){
+    var tempParams ={
+        from_name:document.getElementById(fromName).value ,
+        to_name:document.getElementById(toName).value ,
+        message:document.getElementById(msg).value ,
+    };
+
+    emailjs.send('gmail','template_n4frraq',tempParams)
+    .then(function(res){
+        console.log("success", res.status);
+    })
+}
